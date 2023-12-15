58a7712cf76a8eafe6356b9dc54e0a78daaf80e8
MBEKE <ogadambeke@gmail.com>
2023-12-16 00:25:37 +0300

    Add: 7-print_tebapla.c source code
    The program prints lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
