30df026dda9f890efdcd4dc00cc83703abb35bdc
MBEKE <ogadambeke@gmail.com>
2023-12-20 22:06:48 +0300

    Add: 10-add.c source file
    The function add 2 integers and returns their sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* add - adds 2 intergers and returns the result
+*
+*@a: first interger
+*@b: second integer
+*Return: the result
+*/
+int add(int a, int b)
+{
+	int sum = a  + b;
+
+	return (sum);
+}
