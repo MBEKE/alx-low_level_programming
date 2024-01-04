e188a76e3e560bab8489972c48254d637f6eb035
MBEKE <ogadambeke@gmail.com>
2024-01-04 20:10:11 +0300

    use for loop instead of while loop

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,13 +5,9 @@
 */
 int _strlen(char *s)
 {
-	int length = 0;
-	int i;
+	int length = 0, i;
 
-	while (s[i] != '\0')
-	{
+	for (i = 0; s[i] != '\0'; i++)
 		length++;
-		i++;
-	}
 	return (length);
 }
