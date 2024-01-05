dc785ae54b186689e416ee69a6208bf2a238572d
MBEKE <ogadambeke@gmail.com>
2024-01-05 08:55:34 +0300

    Add: 7- puts_half.c source file
    The function prints half of a string satating with the 1st character

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* puts_half - prints half of a string, followed by a new line
+* @str: the string to be half printed
+* Return: void
+*/
+void puts_half(char *str)
+{
+	int j = 0;
+	int i;
+
+	while (str[j] != '\0')
+		j++;
+	if (j % 2 != 0)
+	{
+		i = (j - 2) / 2;
+		i += 1;
+	}
+	else
+		i = j / 2;
+	for (; i < j; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
