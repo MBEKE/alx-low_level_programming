024155faae3f4ab86bea2383bfa66fccdd7dddf0
MBEKE <ogadambeke@gmail.com>
2024-01-05 19:44:55 +0300

    Add: 6-cap_string.c source file
    The function capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* cap_string - Capitalizes all words of a string.
+* @str: The input string.
+*
+* Return: A pointer to the resulting string.
+*/
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	int i = 0;
+
+	while (ptr[i] != '\0')
+	{
+		if (ptr[i] == ' ' || ptr[i] == '\t' || ptr[i] == '\n' ||
+		    ptr[i] == ',' || ptr[i] == ';' || ptr[i] == '.' ||
+		    ptr[i] == '!' || ptr[i] == '?' || ptr[i] == '"' ||
+		    ptr[i] == '(' || ptr[i] == ')' || 
+		    ptr[i] == '{' || ptr[i] == '}')
+		{
+			if (ptr[i + 1] >= 'a' && ptr[i + 1] <= 'z')
+				ptr[i + 1] -= 'a' - 'A';
+
+
+		else if ((i == 0 || (ptr[i - 1] >= ' ' && ptr[i - 1] <= '}'))
+			&& ptr[i] >= 'a' && ptr[i] <= 'z')
+		{
+			/* the current character is the first character of a word, capitalize it */
+			ptr[i] -= 'a' - 'A';
+		}
+		}
+		i++;
+	}
+	return (str);
+}
+
