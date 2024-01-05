3ad8bc91b06112761f85da6f881c42e9dd5f8586
MBEKE <ogadambeke@gmail.com>
2024-01-05 08:42:00 +0300

    make the file betty compliant

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,7 +6,7 @@
 */
 void rev_string(char *s)
 {
-	int length =0, index = 0;
+	int length = 0, index = 0;
 	char temp;
 
 	while (s[index++])
@@ -14,7 +14,7 @@ void rev_string(char *s)
 	for (index = length - 1; index >= length / 2; index--)
 	{
 		temp = s[index];
-		s[index] = s[length - index -1];
+		s[index] = s[length - index - 1];
 		s[length - index - 1] = temp;
 	}
 }
