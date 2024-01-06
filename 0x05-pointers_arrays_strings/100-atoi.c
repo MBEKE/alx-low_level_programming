a738dd6ac43121e339c4233ea0c2ee52df2bf862
MBEKE <ogadambeke@gmail.com>
2024-01-06 13:36:18 +0300

    debug

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _atoi - converts a sting to integer
+*@s: the input string
+* Return: the converted interger.
+*/
+int _atoi(char *s)
+{
+	int c = 0;
+	unsigned int num = 0;
+	int sign = 1;
+	int isNumeric = 0;
+
+	while (s[c])
+	{
+		if (s[c] == 45)
+			sign = -1;
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isNumeric = 1;
+			num = (num * 10) + (s[c] - '0');
+			c++;
+		}
+		if (isNumeric == 1)
+			break;
+		c++;
+	}
+	num *= sign;
+	return (num);
+}
