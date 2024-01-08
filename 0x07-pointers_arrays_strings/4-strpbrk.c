1968863ff7b1e069463e8260cda768163a359943
MBEKE <ogadambeke@gmail.com>
2024-01-08 18:46:58 +0300

    Add: 4-strpbrk.c source file
    The function searces for a strin for any a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+* _strpbrk - Searches a string foor any of a set of bytes
+* @s: The input string
+* @accept: The set of bytes to search for
+*
+* Return: A pointer to the byte in s that matches on of the
+*bytes in accept or NULL if no such byt is found
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *currentAccept = accept;
+
+		while (*currentAccept != '\0')
+		{
+			if (*s == *currentAccept)
+				return (s);
+			currentAccept++;
+		}
+		s++;
+	}
+	return (NULL);
+	}
