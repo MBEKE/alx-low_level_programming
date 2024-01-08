bb3e4779cd4887c93dfcd18865f6a77d41f03013
MBEKE <ogadambeke@gmail.com>
2024-01-09 01:31:22 +0300

    Add: 5-strstr.c source file
    The function locates a substrig

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+* _strstr - Locates a substring.
+* @haystack: The main strint to search
+* @needle: The substring to find
+* @needle: The substring to find
+*
+* Return: A pointer to the beginning of the located substring
+* or NULL if the substring is not found
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+	char *start_haystack = haystack;
+	char *current_haystack = haystack;
+	char *current_needle = needle;
+
+	while (*current_haystack == *current_needle && *current_needle != '\0')
+	{
+		current_haystack++;
+		current_needle++;
+	}
+	if (*current_needle == '\0')
+		return (start_haystack);
+	haystack++;
+	}
+	return (NULL);
+}
+
+
