3cfe9bf6a5201b456bdfe0acf49d515d84043208
MBEKE <ogadambeke@gmail.com>
2024-01-10 22:25:41 +0300

    Remove while loop and use recursion instead

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int find_sqrt(int n, int guess);
 /**
  * _sqrt_recursion - Returns the natural square root of a number
  * @n: The number to find the square root of.
@@ -9,23 +10,31 @@
  */
 int _sqrt_recursion(int n)
 {
-	int guess = 1;
 	if (n < 0)
 		/* If n is negative, return -1 to indicate an error */
 		return (-1);
 	else if (n == 0 || n == 1)
-		/* Base case: the square root of 0 or 1 is the number itsele */
+		/* Base case: the square root of 0 or 1 is the number itself */
 		return (n);
+	return (find_sqrt(n, 1));
+}
 
-	/**
-	 * Use a while loop to increment the guess untill its square is
-	 * greater than or equal to n
-	 */
-	while (guess * guess < n)
-		guess++;
+/**
+ * find_sqrt - Recursive function to find the square rood.
+ * @n: The number to find the square root of.
+ * @guess: The current guess for the sqaure root.
+ *
+ * Return: The natural sqrt of the given number.
+ * If the number doesn't have a natural sqrt, returns -1 to indicate error
+ */
+int find_sqrt(int n, int guess)
+{
 	if (guess * guess == n)
-		/* Base case: found square root */
+		/* Base case: found the square root */
 		return (guess);
-	/* If the square of the guesss is > n,n has no nat sq */
-	return (-1);
+	else if (guess * guess > n)
+		/* if the sqr of the guess > n, n have no natural sqrt */
+		return (-1);
+	/* make recursive cll to continue searching for sqrt */
+	return (find_sqrt(n, guess + 1));
 }
