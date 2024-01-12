f7f38372082d76ed76f2780b9b71658acc0dd793
MBEKE <ogadambeke@gmail.com>
2024-01-12 22:28:46 +0300

    Add: 0-whatsmyname.c, README.md
    The README.md decribes the how the programs work
    The program in the source file prints its name followed by
    a newline

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints it's name
+ * @argc: The number of arguments
+ * @argv: The argument vector
+ * Return: Always 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
