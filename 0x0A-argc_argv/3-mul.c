c0597c482a0f96edac92291183b7da43e04866f5
MBEKE <ogadambeke@gmail.com>
2024-01-12 23:44:18 +0300

    Debug

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,13 +13,17 @@ int main(int argc, char *argv[])
 	int i;
 	int mult = 1;
 
-	if (argc < 1)
+	if (argc == 3)
+	{
+		for (i = 1; i < argc; i++)
+			mult *= atoi(argv[i]);
+		printf("%d\n", mult);
+	}
+
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	for (i = 1; i < argc; i++)
-		mult *= atoi(argv[i]);
-	printf("%d\n", mult);
 	return (0);
 }
