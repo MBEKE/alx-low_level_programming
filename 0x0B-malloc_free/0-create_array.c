0222ebbe2bb925efe933d1ec9a845b98213b8d8d
MBEKE <ogadambeke@gmail.com>
2024-01-15 22:24:35 +0300

    Add: README.md 0-create_array.c and main.c
    The main.h containt the function protoypes
    
    The 0-create_array.c creates and array of chars, and
    initializes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars, 
+ * and initializes it with a specific char
+ * @size: the number elements in the array to be created
+ * @c: the variable to be stored
+ *
+ * Return: NULL if size is 0, else return the pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arrayPtr;
+	unsigned int count;
+	/* check if size is 0, return NULL if true */
+	if (size == 0)
+		return (NULL);
+	/* Allocate memory for the array */
+	arrayPtr = malloc(size * sizeof(char));
+	/* Check if memory allocation if successful */
+	if (arrayPtr == NULL)
+		return (NULL); 
+	for (count = 0; count < size; count++)
+		arrayPtr[count] = c;
+	return (arrayPtr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,8 @@
+             0x0B-malloc_free
+	 ---------------------
+The tasks in this project are towards understanding the following learnin objectives.
+
+*What is the difference between automatic and dynamic allocation
+*What is malloc and free and how to use them
+*Why and when use malloc
+*How to use valgrind to check for memory leak
