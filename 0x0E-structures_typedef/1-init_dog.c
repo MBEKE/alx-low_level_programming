f73f6b1d8098a4bed928669d6fb02f7ad3b2da83
MBEKE <ogadambeke@gmail.com>
2024-01-22 21:38:30 +0300

    Remove <stdlib.h> library

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
-
 /**
  * init_dog - intiatizes a variable of type struct dog
  * @d: pointer to the struct dog to initialize
@@ -12,10 +10,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
