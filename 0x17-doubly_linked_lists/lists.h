ff9bb0cdc3ce5c0155d51a27c63f70694986550d
MBEKE <ogadambeke@gmail.com>
2024-03-22 01:38:34 +0300

    add: prototype

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -31,6 +31,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 void free_dlistint(dlistint_t *head);
 
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
 void free_dlistint(dlistint_t *head);
 
 int sum_dlistint(dlistint_t *head);
@@ -39,4 +41,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /*LISTS_H */
+#endif
