db5592923fc55d83d6cf390d9006119854dcf7dc
MBEKE <ogadambeke@gmail.com>
2024-03-23 19:23:31 +0300

    docs: Betty style compliance

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,13 +15,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	/* Check for valid inputs */
 	if (array == NULL || cmp == NULL || size <= 0)
-		return(-1);
+		return (-1);
 	/* Iterate thro' the array and find the index for the first match */
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return(i);
+			return (i);
 	}
 	/* Return -1 if no match is found */
-	return(-1);
+	return (-1);
 }
