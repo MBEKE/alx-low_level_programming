03f98f5c28400b9d0423b1c6075b2bbac024136c
MBEKE <ogadambeke@gmail.com>
2024-03-23 17:56:22 +0300

    fix: stop memory leak

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,32 +3,43 @@
 #include <string.h>
 
 /**
- * new_dog - creates a new dog
- * @name:  name of the new dog
- * @age: age of the new dog
- * @owner:  owner of the new dog
- *
- * Return: pointer to the new dog.
- */
+* new_dog - creates a new dog
+* @name: name of the new dog
+* @age: age of the new dog
+* @owner: owner of the new dog
+*
+* Return: pointer to the new dog.
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *name_copy, *owner_copy;
 
 	/* Allocate memory for the new_dog */
 	new_dog = malloc(sizeof(dog_t));
-	/* Check memory allocation failure */
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = strdup(name);
-	new_dog->owner = strdup(owner);
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+
+	/* Make copies of name and owner strings */
+	name_copy = strdup(name);
+	if (name_copy == NULL)
 	{
-		/* free memorry in case of failure */
-		free(new_dog->name);
-		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
+
+	owner_copy = strdup(owner);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
+	}
+
+	/* Assign values to the new_dog struct */
+	new_dog->name = name_copy;
+	new_dog->owner = owner_copy;
 	new_dog->age = age;
+
 	return (new_dog);
 }
