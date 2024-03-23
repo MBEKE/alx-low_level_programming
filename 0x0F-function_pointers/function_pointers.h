86c24719a0022b392a8a1e76a0259845e99cdc10
MBEKE <ogadambeke@gmail.com>
2024-03-23 19:14:31 +0300

    fix: add prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,6 +6,8 @@ void print_name(char *name, void (*f)(char *));
 
 int int_index(int *array, int size, int (*cmp)(int));
 
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 int op_add(int a, int b);
 
 int op_sub(int a, int b);
